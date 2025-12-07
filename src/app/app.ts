317790da942291243b0687ae3f23bffb168f04ec
Luis Sagnay <sagnayluis99@gmail.com>
2025-12-07 18:23:01 -0500

    separacion de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {}
